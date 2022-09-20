439ec4571fc5081c619df8f3f7063a19b3905a7d
unknown <sangvn1@viettel.com.vn>
2022-09-20 18:15:02 +0700

    fix click copy link detail

--- javascripts/discourse/components/vt-copy-link.js
+++ javascripts/discourse/components/vt-copy-link.js
@@ -34,7 +34,7 @@ export default Component.extend(KeyEnterEscape, {
   actions: {
     copyLinkDetail() {
       let currentPath = window.location.href.split('?')[0];
-      if(currentPath == window.location.host) currentPath = `${currentPath}/c/y-tuong/10`
+      if(currentPath == window.location.hostname) currentPath = `${currentPath}/c/y-tuong/10`
       let linkCopy = `${currentPath}?show-modal=true&topic-id=${this.topic.id}`;
       navigator.clipboard.writeText(linkCopy);
     }
